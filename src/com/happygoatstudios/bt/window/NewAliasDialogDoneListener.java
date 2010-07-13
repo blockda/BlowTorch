f021a731e8ae7f57ac99cf6530da2f6e2a19156f
Daniel Block (mac) <blockda@gmail.com>
2010-07-13 09:15:25 -0700

    yes. this will remove those pesky files.

--- src/com/happygoatstudios/bt/window/NewAliasDialogDoneListener.java
+++
@@ -1,6 +0,0 @@
-package com.happygoatstudios.bt.window;
-
-public interface NewAliasDialogDoneListener {
-	public void newAliasDialogDone(String pre,String post);
-	public void editAliasDialogDone(String pre,String post,int pos,String orig);
-}
