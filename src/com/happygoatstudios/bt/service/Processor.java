1182fb22a74adb2a67374f9cc8556ebc7b695ef3
Daniel Block <blockda@gmail.com>
2010-11-15 17:18:44 -0800

    oh mah god so fast.

--- src/com/happygoatstudios/bt/service/Processor.java
+++ src/com/happygoatstudios/bt/service/Processor.java
@@ -40,9 +40,13 @@ public class Processor {
 	Matcher goahead_match = goahead_reg.matcher("");
 	
 	Pattern tab_reg = Pattern.compile("\\x09");
+	
+	
 	Matcher tab_match = tab_reg.matcher("");
 	
-	public Spannable DoProcess(byte[] data) throws UnsupportedEncodingException {
+	Pattern massive_match = Pattern.compile("("+iac_cmd_reg.pattern()+")|" + "("+subnego_reg.pattern()+")|" + "("+goahead_reg.pattern()+")|" + "("+tab_reg.pattern()+")");
+	Matcher ma_matcher = massive_match.matcher("");
+	/*public Spannable DoProcess(byte[] data) throws UnsupportedEncodingException {
 		
 
 
@@ -51,6 +55,77 @@ public class Processor {
 
 		
 		//Matcher iac_match = iac_cmd_reg.matcher(tmp);
+		//instead of doing the 4 pass run, we are going to do the trigger sytle match/lookup
+		ma_matcher.reset(tmp);
+		while(ma_matcher.find()) {
+			//discover what kind of pattern matched.
+			String matched = ma_matcher.group(0);
+			//only thing to do now is to test each one.
+			iac_match.reset(matched);
+			boolean done = false;
+			if(iac_match.matches()) {
+				//dispatch IAC
+				String action = iac_match.group(1);
+				String option = iac_match.group(2);
+				
+				
+				byte[] snd = new byte[3];
+				snd[0] = TC.IAC;
+				byte[] atmp = action.getBytes("ISO-8859-1");
+				snd[1] = atmp[0];
+				atmp = option.getBytes("ISO-8859-1");
+				snd[2] = atmp[0];
+				
+
+				byte[] resp = opthandler.processCommand(snd[0], snd[1], snd[2]);
+				//String response = new String(resp,"ISO-8859-1");
+			
+					
+				String debug_input = TC.getEncodedString(snd);
+				String debug_output = TC.getEncodedString(resp);
+
+				Message sb = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,resp);
+				reportto.sendMessage(sb);
+				done = true;
+			} else {
+				sub_match.reset(matched);
+				if(sub_match.matches()) {
+					//dispatch SUB
+					String subneg = sub_match.group(0);
+					
+					byte[] stmp = subneg.getBytes("ISO-8859-1");
+					
+
+					byte[] sub_r = opthandler.getSubnegotiationResponse(stmp);
+					String sub_resp = new String(opthandler.getSubnegotiationResponse(stmp));
+					
+					//special handling for the compression marker.
+					byte[] compressresp = new byte[1];
+					compressresp[0] = TC.COMPRESS2;
+					if(sub_r[0] == compressresp[0]) {	
+						reportto.sendEmptyMessage(StellarService.MESSAGE_STARTCOMPRESS);
+					} else {
+						Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,sub_r);
+						reportto.sendMessage(sbm);
+					}
+				} else {
+					goahead_match.reset(matched);
+					if(goahead_match.matches()) {
+						//replace goahead.
+					} else {
+						tab_match.reset(matched);
+						if(tab_match.matches()) {
+							//replace tab
+						} else {
+							//we have some kind of option i don't know how to deal with it.
+						}
+					}
+				}
+			}
+			
+		}
+		
+		
 		iac_match.reset(tmp);
 		while(iac_match.find()) {
 			String action = iac_match.group(1);
@@ -71,44 +146,11 @@ public class Processor {
 				
 			String debug_input = TC.getEncodedString(snd);
 			String debug_output = TC.getEncodedString(resp);
-			//Log.e("SERV","OPT: " + debug_input + "|RES: " + debug_output);
-				
-			//try {
-				
-				//service.sendData(resp);
-				//instead send message
+
 				Message sb = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,resp);
-				//Bundle b = new Bundle();
-				//b.putByteArray("THEDATA", resp);
-				//sb.setData(b);
-				
-				/*synchronized(reportto) {
-					while(reportto.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
-						try {
-							reportto.wait();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}*/
-				
+
 				//we have woken
 				reportto.sendMessage(sb);
-			//} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-			//}
-			//send message to reportto now
-			//Message msg = reportto.obtainMessage(BaardTERMService.MESSAGE_SENDDATA);
-			//Bundle b = new Bundle();
-			//b.putByteArray(new Integer(BaardTERMService.MESSAGE_SENDDATA).toString(), resp);
-			//msg.setData(b);
-			//reportto.sendMessage(msg);
-				
-				
-		
-			
 		}
 		
 		//String secondpass = iac_match.replaceAll("");
@@ -131,57 +173,23 @@ public class Processor {
 				//Log.e("PROC","PROCESSOR ENCOUNTERED COMPRESSION START, STARTING COMPRESSION");
 				
 				reportto.sendEmptyMessage(StellarService.MESSAGE_STARTCOMPRESS);
-				
-				//service.
-				//try {
-					//service.beginCompression();
-				//} catch (RemoteException e) {
-					// TODO Auto-generated catch block
-				//	e.printStackTrace();
-				//}
+
 			} else {
 			
 				Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,sub_r);
-				//Bundle b = new Bundle();
-				//b.putByteArray("THEDATA", sub_r);
-				//sbm.setData(b);
-				
-				/*synchronized(reportto) {
-					while(reportto.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
-						try {
-							reportto.wait();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}*/
-				
+
 				reportto.sendMessage(sbm);
-				//Log.e("PROCESS","SLEEPING ON SERVICE.");
-				//synchronized(service) {
-					
-					//service.wait();
-				//}
-				//Log.e("PROCESS","WOKE UP FROM SENDING DATA FROM SERVICE");
 				
 			
 			}
 		}
 		
-		//sigh. gotta avoid displaying the IAC GOAHEAD characters. So I'm gonna have a third pass, because I'm finding it hard to shoehorn it into the other regular expressions.
-		
-		
-		//Matcher third_pass = goahead_reg.matcher(sub_match.replaceAll(""));
+
 		goahead_match.reset(sub_match.replaceAll(""));
 		
 		tab_match.reset(goahead_match.replaceAll(""));
 		
-		//while(third_pass.find()) {
-		//	Log.e("SERVICE","FOUND IAC GOAHEAD");
-		//}
 		
-		//String textdata = sub_match.replaceAll("");
 		String textdata = tab_match.replaceAll("");
 		
 		byte[] textbytes = textdata.getBytes("ISO-8859-1");
@@ -190,15 +198,71 @@ public class Processor {
 		
 		return retval;
 		
-	}
+	}*/
 	
+	StringBuffer holder = new StringBuffer("");
 	public String RawProcess(byte[] data) throws UnsupportedEncodingException {
 		if(data == null) {
 			return "";
 		}
 		String tmp = new String(data,"ISO-8859-1");
-
 		
+		//StringBuffer holder = new StringBuffer("");
+		holder.setLength(0);
+		
+		ma_matcher.reset(tmp);
+		boolean hasmatched = false;
+		while(ma_matcher.find()) {
+			hasmatched = true;
+			String matched = ma_matcher.group(0);
+			boolean tabreplace = false;
+			iac_match.reset(matched);
+			if(iac_match.matches()) {
+				//dispatch IAC
+				//String action = iac_match.group(1);
+				//String option = iac_match.group(2);
+				dispatchIAC(iac_match.group(1),iac_match.group(2));
+			} else {
+				sub_match.reset(matched);
+				if(sub_match.matches()) {
+					//dispatch sub
+					//String subneg = sub_match.group(0);
+					dispatchSUB(sub_match.group(0));
+				} else {
+					goahead_match.reset(matched);
+					if(goahead_match.matches()) {
+						//replace goahead.
+					} else {
+						tab_match.reset(matched);
+						if(tab_match.matches()) {
+							//replace tab
+							tabreplace = true;
+						} else {
+							//unknown, copy through.
+						}
+					}
+				}
+			}
+			if(tabreplace) {
+				ma_matcher.appendReplacement(holder, "    ");
+			} else {
+				ma_matcher.appendReplacement(holder, ""); //remove it from the stream
+			}
+			
+			
+		}
+		if(hasmatched) {
+			ma_matcher.appendTail(holder);
+		} else {
+			holder.append(tmp);
+		}
+		
+		//now we are finished, send text off for dispatching.
+		return holder.toString();
+		
+		/*
+		
+
 		//Matcher iac_match = iac_cmd_reg.matcher(tmp);
 		iac_match.reset(tmp);
 		while(iac_match.find()) {
@@ -220,49 +284,13 @@ public class Processor {
 				
 			String debug_input = TC.getEncodedString(snd);
 			String debug_output = TC.getEncodedString(resp);
-			//Log.e("SERV","OPT: " + debug_input + "|RES: " + debug_output);
-				
-			//try {
-				
-				//service.sendData(resp);
-				//instead send message
-				Message sb = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,resp);
-				//Bundle b = new Bundle();
-				//b.putByteArray("THEDATA", resp);
-				//sb.setData(b);
-				
-				/*synchronized(reportto) {
-					while(reportto.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
-						try {
-							reportto.wait();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}*/
-				
-				//we have woken
-				reportto.sendMessage(sb);
-			//} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-			//}
-			//send message to reportto now
-			//Message msg = reportto.obtainMessage(BaardTERMService.MESSAGE_SENDDATA);
-			//Bundle b = new Bundle();
-			//b.putByteArray(new Integer(BaardTERMService.MESSAGE_SENDDATA).toString(), resp);
-			//msg.setData(b);
-			//reportto.sendMessage(msg);
-				
-				
-		
+
+			Message sb = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,resp);
+
+			reportto.sendMessage(sb);
 			
 		}
-		
-		//String secondpass = iac_match.replaceAll("");
-		
-		//Matcher sub_match = subnego_reg.matcher(secondpass);
+
 		sub_match.reset(iac_match.replaceAll(""));
 		while(sub_match.find()) {
 			String subneg = sub_match.group(0);
@@ -277,79 +305,75 @@ public class Processor {
 			byte[] compressresp = new byte[1];
 			compressresp[0] = TC.COMPRESS2;
 			if(sub_r[0] == compressresp[0]) {
-				//Log.e("PROC","PROCESSOR ENCOUNTERED COMPRESSION START, STARTING COMPRESSION");
-				
-				//reportto.sendEmptyMessage(StellarService.MESSAGE_STARTCOMPRESS);
 				reportto.sendMessageAtFrontOfQueue(reportto.obtainMessage(StellarService.MESSAGE_STARTCOMPRESS));
-				
-				
-				
-				//service.
-				//try {
-					//service.beginCompression();
-				//} catch (RemoteException e) {
-					// TODO Auto-generated catch block
-				//	e.printStackTrace();
-				//}
+
 			} else {
-			
 				Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,sub_r);
-				//Bundle b = new Bundle();
-				//b.putByteArray("THEDATA", sub_r);
-				//sbm.setData(b);
-				
-				/*synchronized(reportto) {
-					while(reportto.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
-						try {
-							reportto.wait();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}*/
-				
 				reportto.sendMessage(sbm);
-				//Log.e("PROCESS","SLEEPING ON SERVICE.");
-				//synchronized(service) {
-					
-					//service.wait();
-				//}
-				//Log.e("PROCESS","WOKE UP FROM SENDING DATA FROM SERVICE");
-				
-			
+
 			}
 		}
 		
-		//Matcher third_pass = goahead_reg.matcher(sub_match.replaceAll(""));
+	
 		goahead_match.reset(sub_match.replaceAll(""));
-		//while(third_pass.find()) {
-		//	Log.e("SERVICE","FOUND IAC GOAHEAD");
-		//}
-		
-		//String textdata = sub_match.replaceAll("");
-		
-		//Matcher fourth_pass = tab_reg.matcher(third_pass.replaceAll(""));
+
 		tab_match.reset(goahead_match.replaceAll(""));
 		
 		String textdata = tab_match.replaceAll("    ");
 		
-		//if(textdata.contains(new Character((char)0x09).toString())) {
-		//	Log.e("SERVICE","FOUND TAB");
-		//	textdata.replaceAll("\\x09", "    ");
-		//}
-		//Integer i = new Integer(0);
-		//for(i=0;i<0x1F;i++) {
-			
-		//	if(textdata.contains(new Character((char) i.byteValue()).toString())) {
-		//		Log.e("SERVICE","FOUND SPECIAL CHAR: " + Integer.toHexString(i));
-		//	}
-		//}
+
 		
 		
 		byte[] textbytes = textdata.getBytes("ISO-8859-1");
 		
-		return textdata;
+		return textdata;*/
+	}
+	
+	public void dispatchIAC(String action,String option) throws UnsupportedEncodingException {
+		//String action = iac_match.group(1);
+		//String option = iac_match.group(2);
+		
+		
+		byte[] snd = new byte[3];
+		snd[0] = TC.IAC;
+		byte[] atmp = action.getBytes("ISO-8859-1");
+		snd[1] = atmp[0];
+		atmp = option.getBytes("ISO-8859-1");
+		snd[2] = atmp[0];
+		
+
+		byte[] resp = opthandler.processCommand(snd[0], snd[1], snd[2]);
+		//String response = new String(resp,"ISO-8859-1");
+	
+			
+		//String debug_input = TC.getEncodedString(snd);
+		//String debug_output = TC.getEncodedString(resp);
+
+		Message sb = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,resp);
+
+		reportto.sendMessage(sb);
+	}
+	
+	public void dispatchSUB(String negotiation) throws UnsupportedEncodingException {
+		//String subneg = sub_match.group(0);
+		
+		byte[] stmp = negotiation.getBytes("ISO-8859-1");
+		
+
+		byte[] sub_r = opthandler.getSubnegotiationResponse(stmp);
+		String sub_resp = new String(opthandler.getSubnegotiationResponse(stmp));
+		
+		//special handling for the compression marker.
+		byte[] compressresp = new byte[1];
+		compressresp[0] = TC.COMPRESS2;
+		if(sub_r[0] == compressresp[0]) {
+			reportto.sendMessageAtFrontOfQueue(reportto.obtainMessage(StellarService.MESSAGE_STARTCOMPRESS));
+
+		} else {
+			Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,sub_r);
+			reportto.sendMessage(sbm);
+
+		}
 	}
 	
 
