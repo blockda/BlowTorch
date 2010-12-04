455bf9116c663eea3482770e9c41baee9bcd8b3a
Dan Block (spike) <blockda@gmail.com>
2010-12-04 15:04:22 -0800

    Wierd line break update

--- src/com/happygoatstudios/bt/service/Processor.java
+++ src/com/happygoatstudios/bt/service/Processor.java
@@ -1,9 +1,7 @@
 package com.happygoatstudios.bt.service;
 
-
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-
 
 import android.os.Handler;
 import android.os.Message;
@@ -14,74 +12,79 @@ import android.os.Message;
 import android.util.Log;
 
 public class Processor {
-	
+
 	Handler reportto = null;
 	Colorizer colormebad = new Colorizer();
 	OptionNegotiator opthandler;
 	IStellarService.Stub service = null;
-	
+
 	private String encoding = null;
-	
-	public Processor(Handler useme,IStellarService.Stub theserv,String pEncoding) {
+
+	public Processor(Handler useme, IStellarService.Stub theserv,
+			String pEncoding) {
 		reportto = useme;
 		service = theserv;
-		
+
 		opthandler = new OptionNegotiator();
-		
+
 		setEncoding(pEncoding);
 	}
-	
+
 	private final boolean debugTelnet = false;
-	
+
 	private final byte IAC = (byte) 0xFF;
 	private final byte SB = (byte) 0xFA;
-	private final byte SE = (byte)0xF0;
-	
+	private final byte SE = (byte) 0xF0;
+
 	private final byte WILL = (byte) 0xFB;
-	//private final byte WONT = (byte) 0xFC;
-	//private final byte DO = (byte) 0xFD;
+	// private final byte WONT = (byte) 0xFC;
+	// private final byte DO = (byte) 0xFD;
 	private final byte DONT = (byte) 0xFE;
-	
-	private final byte  BREAK = (byte)243;    //NVT character BRK.
-	//private final byte Interrupt Process   244    //The function IP.
-	private final byte  AO  =(byte)245;    //The function AO.
-	private final byte AYT =(byte)246;    //The function AYT.
-	private final byte EC = (byte)247;    //The function EC.
-	private final byte  EL = (byte)248;    //The function EL.
-	
-	private final byte GOAHEAD = (byte)0xF9;
-	
-	private final byte IP = (byte)0xF4;
-	
-	//private final byte TAB = (byte)0x09;
-	private final byte BELL = (byte)0x07;
-	//private byte
-	//subnegotioation: \\xFF\\xFA(.{1})(.*)\\xFF\\xF0
+
+	private final byte BREAK = (byte) 243; // NVT character BRK.
+	// private final byte Interrupt Process 244 //The function IP.
+	private final byte AO = (byte) 245; // The function AO.
+	private final byte AYT = (byte) 246; // The function AYT.
+	private final byte EC = (byte) 247; // The function EC.
+	private final byte EL = (byte) 248; // The function EL.
+
+	private final byte GOAHEAD = (byte) 0xF9;
+
+	private final byte IP = (byte) 0xF4;
+
+	// private final byte TAB = (byte)0x09;
+	private final byte BELL = (byte) 0x07;
+
+	// private byte
+	// subnegotioation: \\xFF\\xFA(.{1})(.*)\\xFF\\xF0
 
 	public String RawProcess(byte[] data) {
-		if(data == null) {
+		if (data == null) {
 			return "";
 		}
-		
+
 		ByteBuffer buff = ByteBuffer.allocate(data.length);
 		ByteBuffer opbuf = ByteBuffer.allocate(30);
 
-		int count = 0; //count of the number of bytes in the buffer;
-		for(int i=0;i<data.length;i++) {
-			switch(data[i]) {
+		int count = 0; // count of the number of bytes in the buffer;
+		for (int i = 0; i < data.length; i++) {
+			switch (data[i]) {
 			case IAC:
-				//if the next byte is
-				if((data[i+1] >= WILL && data[i+1] <= DONT) || data[i+1] == SB) {
-					Log.e("SERVICE","DO IAC");
-					//switch(data[i+1]) 
-					if(data[i+1] == SB) {
-						//subnegotiation
-						//now we have an optional number of bytes between the indicated subnegotiation and the IAC SE end of sequence.
+				// if the next byte is
+				if ((data[i + 1] >= WILL && data[i + 1] <= DONT)
+						|| data[i + 1] == SB) {
+					Log.e("SERVICE", "DO IAC");
+					// switch(data[i+1])
+					if (data[i + 1] == SB) {
+						// subnegotiation
+						// now we have an optional number of bytes between the
+						// indicated subnegotiation and the IAC SE end of
+						// sequence.
 						boolean done = false;
-						int j = i+3;
-						while(!done) {
-							if(data[j] == IAC) {
-								if(data[j+1] == SE) {
+						int j = i + 3;
+						while (!done) {
+							if (data[j] == IAC) {
+								if (data[j + 1] == SE) {
 									done = true;
 								}
 							} else {
@@ -89,113 +92,130 @@ public class Processor {
 								j++;
 							}
 						}
-						//so if we are here, than j - (i+3)  is the number of optional bytes.
-						opbuf = ByteBuffer.allocate(j-(i+3) + 5);
+						// so if we are here, than j - (i+3) is the number of
+						// optional bytes.
+						opbuf = ByteBuffer.allocate(j - (i + 3) + 5);
 						opbuf.put(IAC);
-						opbuf.put(data[i+1]);
-						opbuf.put(data[i+2]);
-						if(j-(i+3) > 0) {
-							for(int q=i+3;q<j;q++) {
+						opbuf.put(data[i + 1]);
+						opbuf.put(data[i + 2]);
+						if (j - (i + 3) > 0) {
+							for (int q = i + 3; q < j; q++) {
 								opbuf.put(data[q]);
 							}
 						}
 						opbuf.put(IAC);
 						opbuf.put(SE);
-						
-						
+
 						opbuf.rewind();
 						boolean compress = dispatchSUB(opbuf.array());
-						if(compress) {
-							ByteBuffer b = ByteBuffer.allocate(data.length - 5 - i);
-							//if(in[0] == IAC && in[1] == SB && in[2] == compressresp[0] && in[3] == IAC && in[4] == SE) {
-								Log.e("PROCESSOR","ENCOUNTERED START OF COMPRESSION EVENT");
-								//get rest
-								
-							for(int z=i+5;z<data.length;z++) {
+						if (compress) {
+							ByteBuffer b = ByteBuffer.allocate(data.length - 5
+									- i);
+							// if(in[0] == IAC && in[1] == SB && in[2] ==
+							// compressresp[0] && in[3] == IAC && in[4] == SE) {
+							Log.e("PROCESSOR",
+									"ENCOUNTERED START OF COMPRESSION EVENT");
+							// get rest
+
+							for (int z = i + 5; z < data.length; z++) {
 								b.put(data[z]);
 							}
-								
-							
+
 							b.rewind();
-							reportto.sendMessageAtFrontOfQueue(reportto.obtainMessage(StellarService.MESSAGE_STARTCOMPRESS,b.array()));
+							reportto.sendMessageAtFrontOfQueue(reportto
+									.obtainMessage(
+											StellarService.MESSAGE_STARTCOMPRESS,
+											b.array()));
 							byte[] trunc = new byte[count];
 							buff.rewind();
-							buff.get(trunc,0,count);
+							buff.get(trunc, 0, count);
 							try {
-								return new String(trunc,encoding);
+								return new String(trunc, encoding);
 							} catch (UnsupportedEncodingException e) {
 								throw new RuntimeException(e);
 							}
-						
+
 						} else {
-							i = i + 2 + (j-(i+3)) + 2; // (original pos, plus the 2 mandatory bytes, plus the optional data length, plus the 2 bytes at the end (one is included in the loop).
+							i = i + 2 + (j - (i + 3)) + 2; // (original pos,
+															// plus the 2
+															// mandatory bytes,
+															// plus the optional
+															// data length, plus
+															// the 2 bytes at
+															// the end (one is
+															// included in the
+															// loop).
 						}
 					} else {
-						dispatchIAC(data[i+1],data[i+2]);
+						dispatchIAC(data[i + 1], data[i + 2]);
 						i = i + 2;
 					}
-				} else {//if(data[i+1] == GOAHEAD) {
-				
-					//i++;
-					//Log.e("SERVICE","DO GOAHEAD");
-				//} else if(data[i+1] == IAC) {
-					///Log.e("SERVICE","FOUND IAC AS DATA");
-					//buff.put(data[i]);
-					//count++;
-					//i++;
-				//} else if(data[i+1] == IP)  {
-					//we handle disconnections on our own.
-					//Log.e("SERVICE","GOT IP");
-					//i++;
-					
-					switch(data[i+1]) {
+				} else {// if(data[i+1] == GOAHEAD) {
+
+					// i++;
+					// Log.e("SERVICE","DO GOAHEAD");
+					// } else if(data[i+1] == IAC) {
+					// /Log.e("SERVICE","FOUND IAC AS DATA");
+					// buff.put(data[i]);
+					// count++;
+					// i++;
+					// } else if(data[i+1] == IP) {
+					// we handle disconnections on our own.
+					// Log.e("SERVICE","GOT IP");
+					// i++;
+
+					switch (data[i + 1]) {
 					case IAC:
-						buff.put(data[i]); //and one IAC and consume the extra.
+						buff.put(data[i]); // and one IAC and consume the extra.
 						count++;
 						break;
 					case GOAHEAD:
 					case IP:
-						//TODO: REAL IP HANDLING HERE, I THINK THIS INVOLVES SETTING THE CURSOR BACK TO A PLACE OR SOMETHING
+						// TODO: REAL IP HANDLING HERE, I THINK THIS INVOLVES
+						// SETTING THE CURSOR BACK TO A PLACE OR SOMETHING
 					case BREAK:
 					case AO:
-						//i think this one is more for us to send to the server.
+						// i think this one is more for us to send to the
+						// server.
 					case EC:
-						//TODO: REAL ERASE CHARACTER 
+						// TODO: REAL ERASE CHARACTER
 					case EL:
-						//TODO: REAL ERASE LINE
+						// TODO: REAL ERASE LINE
 					case AYT:
-						i++; //consume the byte.
+						i++; // consume the byte.
 						break;
 					default:
-						//everything else keep
+						// everything else keep
 						break;
 					}
 				}
 				break;
 			case BELL:
-				//dispatch bell
+				// dispatch bell
 				reportto.sendEmptyMessage(StellarService.MESSAGE_BELLINC);
 				break;
-			//UNTIL FURTHER NOTICE, TAB HANDLING WILL BE THE WINDOWS RESPONSIBILITY
+			// UNTIL FURTHER NOTICE, TAB HANDLING WILL BE THE WINDOWS
+			// RESPONSIBILITY
 			default:
-				buff.put(data[i]);	
+				buff.put(data[i]);
 				count++;
 				break;
 			}
-			
+
 		}
-		//buff.rewind();
-		//count should reflect an accurate amount of bytes written to the buffer.
+		// buff.rewind();
+		// count should reflect an accurate amount of bytes written to the
+		// buffer.
 		buff.rewind();
 		byte[] tmp = new byte[count];
-		buff.get(tmp,0,count);
+		buff.get(tmp, 0, count);
 		try {
-			return new String(tmp,encoding);
+			return new String(tmp, encoding);
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public void dispatchIAC(byte action,byte option) {
 		
 		//Log.e("PROCESSOR","GOT COMMAND:" + "IAC|" + TC.decodeInt(new String(new byte[]{action},encoding),encoding) + "|"+ TC.decodeInt(new String(new byte[]{option},encoding), encoding));
@@ -205,33 +225,37 @@ public class Processor {
 		
 		reportto.sendMessage(sb);
 	}
-	
+
 	public boolean dispatchSUB(byte[] negotiation) {
-		//byte[] stmp = negotiation.getBytes("ISO-8859-1");
-		//Log.e("PROCESSOR","GOT SUBNEGOTIATION:" + TC.decodeInt(new String(negotiation,encoding), encoding));
+		// byte[] stmp = negotiation.getBytes("ISO-8859-1");
+		// Log.e("PROCESSOR","GOT SUBNEGOTIATION:" + TC.decodeInt(new
+		// String(negotiation,encoding), encoding));
 
 		byte[] sub_r = opthandler.getSubnegotiationResponse(negotiation);
-		//String sub_resp = new String(opthandler.getSubnegotiationResponse(stmp));
-		
-		if(sub_r == null) {
-			//Log.e("PROCESSOR","SUBNEGOTIATION RESPONSE NULL");
+		// String sub_resp = new
+		// String(opthandler.getSubnegotiationResponse(stmp));
+
+		if (sub_r == null) {
+			// Log.e("PROCESSOR","SUBNEGOTIATION RESPONSE NULL");
 			return false;
 		} else {
-			
-			//Log.e("PROCESSOR","RESPONSE:" + TC.decodeInt(new String(sub_r,encoding), encoding));
+
+			// Log.e("PROCESSOR","RESPONSE:" + TC.decodeInt(new
+			// String(sub_r,encoding), encoding));
 		}
 
-		//special handling for the compression marker.
+		// special handling for the compression marker.
 		byte[] compressresp = new byte[1];
 		compressresp[0] = TC.COMPRESS2;
-		
-		if(sub_r[0] == compressresp[0]) {
+
+		if (sub_r[0] == compressresp[0]) {
 			return true;
 		} else {
-			Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,sub_r);
+			Message sbm = reportto.obtainMessage(
+					StellarService.MESSAGE_SENDOPTIONDATA, sub_r);
 			reportto.sendMessage(sbm);
 			return false;
-		} 
+		}
 	}
 
 	public void setEncoding(String encoding) {
@@ -241,60 +265,44 @@ public class Processor {
 	public String getEncoding() {
 		return encoding;
 	}
-	
-	public void setDisplayDimensions(int rows,int cols) {
+
+	public void setDisplayDimensions(int rows, int cols) {
 		opthandler.setColumns(cols);
 		opthandler.setRows(rows);
 	}
-	
+
 	public void disaptchNawsString() {
-		if(opthandler.getNawsString() == null) return;
-		Message sbm = reportto.obtainMessage(StellarService.MESSAGE_SENDOPTIONDATA,opthandler.getNawsString());
+		if (opthandler.getNawsString() == null)
+			return;
+		Message sbm = reportto.obtainMessage(
+				StellarService.MESSAGE_SENDOPTIONDATA,
+				opthandler.getNawsString());
 		reportto.sendMessage(sbm);
 		return;
 	}
-	
+
 	public void reset() {
 		opthandler.reset();
 	}
-	
 
 }
 
-/* Straight from rfc 854
- *       NAME               CODE              MEANING
-
-      SE                  240    End of subnegotiation parameters.
-      NOP                 241    No operation.
-      Data Mark           242    The data stream portion of a Synch.
-                                 This should always be accompanied
-                                 by a TCP Urgent notification.
-      Break               243    NVT character BRK.
-      Interrupt Process   244    The function IP.
-      Abort output        245    The function AO.
-      Are You There       246    The function AYT.
-      Erase character     247    The function EC.
-      Erase Line          248    The function EL.
-      Go ahead            249    The GA signal.
-      SB                  250    Indicates that what follows is
-                                 subnegotiation of the indicated
-                                 option.
-      WILL (option code)  251    Indicates the desire to begin
-                                 performing, or confirmation that
-                                 you are now performing, the
-                                 indicated option.
-      WON'T (option code) 252    Indicates the refusal to perform,
-                                 or continue performing, the
-                                 indicated option.
-      DO (option code)    253    Indicates the request that the
-                                 other party perform, or
-                                 confirmation that you are expecting
-                                 the other party to perform, the
-                                 indicated option.
-      DON'T (option code) 254    Indicates the demand that the
-                                 other party stop performing,
-                                 or confirmation that you are no
-                                 longer expecting the other party
-                                 to perform, the indicated option.
-      IAC                 255    Data Byte 255.
-*/
+/*
+ * Straight from rfc 854 NAME CODE MEANING
+ * 
+ * SE 240 End of subnegotiation parameters. NOP 241 No operation. Data Mark 242
+ * The data stream portion of a Synch. This should always be accompanied by a
+ * TCP Urgent notification. Break 243 NVT character BRK. Interrupt Process 244
+ * The function IP. Abort output 245 The function AO. Are You There 246 The
+ * function AYT. Erase character 247 The function EC. Erase Line 248 The
+ * function EL. Go ahead 249 The GA signal. SB 250 Indicates that what follows
+ * is subnegotiation of the indicated option. WILL (option code) 251 Indicates
+ * the desire to begin performing, or confirmation that you are now performing,
+ * the indicated option. WON'T (option code) 252 Indicates the refusal to
+ * perform, or continue performing, the indicated option. DO (option code) 253
+ * Indicates the request that the other party perform, or confirmation that you
+ * are expecting the other party to perform, the indicated option. DON'T (option
+ * code) 254 Indicates the demand that the other party stop performing, or
+ * confirmation that you are no longer expecting the other party to perform, the
+ * indicated option. IAC 255 Data Byte 255.
+ */
