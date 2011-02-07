38f7d5c99e8de9a8b4aa91e90ba940b2163dc4c7
Daniel Block <blockda@gmail.com>
2011-02-06 21:30:35 -0800

    Omfg chainsaw hack of hackerton.

---
+++ BT_Test/src/com/happygoatstudios/bttest/TestLauncher.java
@@ -0,0 +1,21 @@
+package com.happygoatstudios.bttest;
+
+import com.happygoatstudios.bt.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TestLauncher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        
+        Intent launch = new Intent(this,com.happygoatstudios.bt.launcher.Launcher.class);
+        launch.putExtra("LAUNCH_MODE","com.happygoatstudios.bttest");
+        this.startActivity(launch);
+        
+        this.finish();
+    }
+}
