0bddc68c1f67b4a2fb558a9208da5af1789dff16
Daniel Block <blockda@gmail.com>
2011-02-23 16:46:38 -0800

    Adding graphics sources and archive versions for consolidation purposes.

---
+++ archive/BaardTERM/src/com/happygoatstudios/bt/window/SlickButton.java
@@ -0,0 +1,368 @@
+package com.happygoatstudios.bt.window;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class SlickButton extends View {
+
+	//int x;
+	//int y;
+	boolean hasfocus = false;
+	private Listener the_listener = new Listener();
+	private Handler myhandler = null;
+	//private String the_text = null;
+	//private String the_label = null;
+	private SlickButtonData data = new SlickButtonData();
+	private Handler dispatcher = null;
+	private Handler deleter = null;
+	private int size = 80;
+	
+	boolean moving = false;
+	
+	final static public int MSG_BEGINMOVE = 100;
+	final static public int MSG_DELETE = 101;
+	
+	public SlickButton(Context context,int px,int py) {
+		super(context);
+		//x = px;
+		//y = py;
+		data.x = px;
+		data.y = py;
+		this.setClickable(true);
+		this.setFocusable(true);
+		this.setOnClickListener(the_listener);
+		data.the_label = "NULL!";
+		
+		myhandler = new Handler() {
+			public void handleMessage(Message msg) {
+				switch(msg.what) {
+				case MSG_BEGINMOVE:
+					moving = true;
+					SlickButton.this.invalidate();
+					break;
+				case MSG_DELETE:
+					Message deleme = deleter.obtainMessage(666, SlickButton.this);
+					deleter.sendMessage(deleme);
+					//dispatcher.
+					break;
+				}
+			}
+		};
+		
+		
+		Log.e("SB","SLICKBUTTON CONSTRUCTOR PASSED");
+		
+	}
+	
+	public void setData(SlickButtonData in) {
+		data = in;
+	}
+	
+	public SlickButtonData getData() {
+		return data;
+	}
+	
+
+	public void setDeleter(Handler d) {
+		deleter = d;
+	}
+	
+	public void setDispatcher(Handler d) {
+		dispatcher = d;
+	}
+	
+	public void doDispatchFlip() {
+		//only dispatch flip if it contains a command
+		if(data.flip_command.equals("")) {
+			
+		} else {
+			dispatchText(data.flip_command);
+		}
+	}
+	
+	public void doDispatch() {
+		//Message tmp = ConnectionHandler.obtainMessage(105);
+		dispatchText(data.the_text);
+		
+	}
+	
+	public void dispatchText(String istr) {
+		//Log.e("SB","DISPATCHING " + istr);
+		String tmp = istr;
+		//history.addCommand(data);
+		Character cr = new Character((char)13);
+		Character lf = new Character((char)10);
+		String crlf = cr.toString() + lf.toString();
+		tmp = tmp.concat(crlf);
+		ByteBuffer buf = ByteBuffer.allocate(tmp.length());
+	
+		try {
+			buf.put(tmp.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		}
+	
+		buf.rewind();
+	
+		byte[] buffbytes = buf.array();
+		
+		Message msg = dispatcher.obtainMessage(105);
+		Bundle b = msg.getData();
+		b.putByteArray("DATA", buffbytes);
+		msg.setData(b);
+		
+		dispatcher.sendMessage(msg);
+	}
+	
+	public void setText(String t) {
+		data.the_text = t;
+	}
+	
+	public String getText() {
+		return data.the_text;
+	}
+	
+	public String getLabel() {
+		return data.the_label;
+	}
+	
+	public void setFlipCommand(String t) {
+		data.flip_command = t;
+	}
+	
+	public void setMoveMethod(int i) {
+		//Log.e("SB","MOVE STATE CHANGED TO:"+i);
+		data.MOVE_STATE = i;
+	}
+	
+	public int getMoveMethod() {
+		return data.MOVE_STATE;
+	}
+	
+	public void setLabel(String t) {
+		if(t == null) {
+			t = "NULL!";
+		}
+		data.the_label = t;
+	}
+	
+	private int start_x = 0;
+	private int start_y = 0;
+	
+	private int save_x = 0;
+	private int save_y = 0;
+	
+	boolean nudged = false;
+	boolean button_down = false;
+	
+	public boolean onTouchEvent(MotionEvent e) {
+		
+		//if(!nudged) {
+		//	save_x = data.x;
+		//	save_y = data.y;
+		//}
+		int pointer = e.getPointerId(0);
+		int touchx = (int) e.getX(pointer);
+		int touchy = (int) e.getY(pointer);
+		
+		Rect rect = new Rect();
+		rect.set(data.x-(size/2),data.y-(size/2),data.x+(size/2),data.y+(size/2));
+		
+		if(rect.contains(touchx,touchy)) {
+			//continue
+		} else {
+			if(!moving && !button_down) {
+				return false;
+			}
+			
+			if(!button_down) {
+				return false;
+			} else {
+				//Log.e("SB","LETTING CLICK PASS THROUGH BECAUSE BUTTON IS DOWN");
+			}
+		}
+		
+		//Log.e("SB","SB GOT TOUCH EVENT");
+		if(e.getAction() == MotionEvent.ACTION_DOWN) {
+			hasfocus = true;
+			start_x = touchx;
+			start_y = touchy;
+			//schedule message for moving
+			myhandler.sendEmptyMessageDelayed(MSG_BEGINMOVE, 1500);
+			myhandler.sendEmptyMessageDelayed(MSG_DELETE, 4000);
+			save_x = data.x;
+			save_y = data.y;
+			button_down=true;
+			this.invalidate();
+		}
+		if(e.getAction() == MotionEvent.ACTION_MOVE) {
+			if(moving) {
+				if(data.MOVE_STATE == data.MOVE_FREE) {
+					data.x = touchx;
+					data.y = touchy;
+				} else if (data.MOVE_STATE == data.MOVE_NUDGE) {
+					//compute nudge
+					int tmpx = touchx - start_x;
+					int tmpy = touchy - start_y;
+					data.x = save_x + tmpx / 10;
+					data.y = save_y + tmpy / 10;
+					//double dist = 
+					nudged = true;
+				}
+			}
+			int diff_x = touchx - start_x;
+			int diff_y = touchy - start_y;
+			double abs_length = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y, 2));
+			if(abs_length > 18.0) {
+				//Log.e("SLICK","Length: " + (new Double(abs_length)));
+				myhandler.removeMessages(MSG_DELETE);
+			}
+			this.invalidate();
+		}
+		
+		if(e.getAction() == MotionEvent.ACTION_UP) {
+			hasfocus = false;
+			myhandler.removeMessages(MSG_BEGINMOVE);
+			myhandler.removeMessages(MSG_DELETE);
+			if(!moving) {
+				if(!rect.contains(touchx,touchy)) {
+					//up action happend outside of recticle, do dispatch flip
+					doDispatchFlip(); //execute flip command
+				} else {
+					doDispatch();
+				}
+			}
+			
+			button_down = false;
+			
+			moving = false;
+			nudged = false;
+			
+			this.invalidate();
+		}
+		return true;
+	}
+	
+	protected void onFocusChanged(boolean gainFocus,int direction,Rect prev_rect) {
+		//Log.e("SB","FOCUS CHANGED");
+		if(gainFocus == true) {
+			hasfocus = true;
+		} else {
+			hasfocus = false;
+		}
+		this.invalidate();
+	}
+	
+	public void onDraw(Canvas c) {
+		Rect rect = new Rect();
+		
+		rect.set(data.x-(size/2),data.y-(size/2),data.x+(size/2),data.y+(size/2));
+		//RectF f_rect = new RectF(rect);
+		//c.drawColor(0xFF0FF000);
+		Paint p = new Paint();
+		
+		if(hasfocus) {
+			p.setColor(0x8800FF00);
+		} else {
+			p.setColor(0x550000FF);
+		}
+		//c.drawRoundRect(f_rect, 8,8, p);
+		c.drawRect(rect, p);
+		
+		//get text size.
+		Paint opts = new Paint();
+		opts.setTypeface(Typeface.DEFAULT_BOLD);
+		opts.setColor(0x990000FF);
+		opts.setTextSize(24);
+		float tsize = opts.measureText(data.the_label);
+		c.drawText(data.the_label, data.x-tsize/2, data.y+12, opts);
+		
+		if(moving) {
+			Rect m_rect = new Rect();
+			m_rect.set(data.x-(size/2)+5,data.y-(size/2)+5,data.x+(size/2)-5,data.y+(size/2)-5);
+			Paint rpaint = new Paint();
+			rpaint.setColor(0xAAFF0000);
+			c.drawRect(m_rect, rpaint);
+			//RectF frect = new RectF(m_rect);
+			//c.drawRoundRect(frect, 8, 8, rpaint);
+		}
+		
+		return;
+	}
+	
+	public void setOnClickListener(OnClickListener l) {
+		the_listener.setListener(l);
+	}
+	
+	
+	protected void onMeasure(int measurespec) {
+		this.setMeasuredDimension(measureWidth(measurespec), measureHeight(measurespec));
+	}
+	
+	private int measureWidth(int measureSpec) {
+		int pref = 50;
+		return getMesaurement(measureSpec,pref);
+	}
+	
+	private int measureHeight(int measureSpec) {
+		int pref = 50;
+		return getMesaurement(measureSpec,pref);
+	}
+	
+	private int getMesaurement(int measureSpec,int pref) {
+		int specSize = MeasureSpec.getSize(measureSpec);
+		int measurement = 0;
+		
+		switch(MeasureSpec.getMode(measureSpec)) {
+		case MeasureSpec.EXACTLY:
+			measurement = specSize;
+			break;
+		case MeasureSpec.AT_MOST:
+			measurement = Math.min(specSize, pref);
+			break;
+		default:
+			measurement = pref;
+			break;
+		}
+		
+		return measurement;
+	}
+	
+	private class Listener implements View.OnClickListener 
+	{
+		private OnClickListener listener = null;
+
+		public void onClick(View v) {
+			//Log.e("SB","SENDING CLICK PRESS");
+			if(listener != null) {
+				listener.onClick(SlickButton.this);
+			}
+		}
+		
+		public void setListener(OnClickListener l) {
+			listener = l;
+		}
+		
+		
+	}
+	
+
+
+}
---
+++ archive/BaardTERM_bakcup/src/com/happygoatstudios/bt/service/BaardTERMService.java
@@ -0,0 +1,687 @@
+package com.happygoatstudios.bt.service;
+
+
+
+
+import java.io.BufferedOutputStream;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.happygoatstudios.bt.service.IBaardTERMServiceCallback;
+import com.happygoatstudios.bt.service.IBaardTERMService;
+
+
+public class BaardTERMService extends Service {
+
+	final RemoteCallbackList<IBaardTERMServiceCallback> callbacks = new RemoteCallbackList<IBaardTERMServiceCallback>();
+	
+	NotificationManager mNM;
+	
+	OutputStream output_writer = null;
+	
+	OutputWriter outputter = null;
+	
+	Processor the_processor = null;
+	
+	Object sendlock = new Object();
+	
+	protected int bindCount = 0;
+	
+	InetAddress the_addr = null;
+	String host;
+	int port;
+	final int BAD_PORT = 999999;
+	final String BAD_HOST = "NOTSETYET";
+	Socket the_socket = null;
+	
+	DataPumper pump = null;
+	
+	Handler myhandler = null;
+	
+	public int trigger_count = 5555;
+	
+	final static public int MESSAGE_PROCESS = 102;
+	final static public int MESSAGE_INIT = 100;
+	final static public int MESSAGE_END = 101;
+	final static public int MESSAGE_SETDATA = 103;
+	final static public int MESSAGE_STARTCOMPRESS = 104;
+	final static public int MESSAGE_ENDCOMPRESS = 105;
+	final static public int MESSAGE_SENDDATA = 106;
+	final static public int MESSAGE_REQUESTBUFFER = 107;
+	final static public int MESSAGE_CHECKIFALIVE = 109;
+
+	protected static final int MESSAGE_SAVEBUFFER = 108;
+	
+	public boolean sending = false;
+	
+	StringBuffer the_buffer = new StringBuffer();
+	
+	
+	public void onCreate() {
+		//called when we are created from a startService or bindService call with the IBaardTERMService interface intent.
+		Log.e("SERV","BAARDTERMSERVICE STARTING!");
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		host = BAD_HOST;
+		port = BAD_PORT;
+		
+		//Looper.prepare();
+		
+		myhandler = new Handler() {
+			public void handleMessage(Message msg) {
+				switch(msg.what) {
+				case MESSAGE_INIT:
+					try {
+						doStartup();
+					} catch (SocketException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case MESSAGE_END:
+					pump.stop();
+					doShutdown();
+					break;
+				case MESSAGE_PROCESS:
+					byte[] data = msg.getData().getByteArray("THEBYTES");
+					//need to throttle this somehow to avoid sending messages too fast.
+					try {
+						dispatch(data);
+					} catch (RemoteException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case MESSAGE_SETDATA:
+					host = msg.getData().getString("HOST");
+					port = msg.getData().getInt("PORT");
+					break;
+				case MESSAGE_STARTCOMPRESS:
+					pump.getHandler().sendEmptyMessage(DataPumper.MESSAGE_COMPRESS);
+					break;
+				case MESSAGE_ENDCOMPRESS:
+					break;
+				case MESSAGE_SENDDATA:
+					//Message dmsg = outputter.getHandler().obtainMessage(102);
+					//Bundle bun = new Bundle();
+					//bun.putByteArray("THEDATA", msg.getData().getByteArray("THEDATA"));
+					//dmsg.setData(bun);
+					//outputter.getHandler().sendMessage(dmsg);
+					
+					byte[] bytes = msg.getData().getByteArray("THEDATA");
+					
+					
+					
+					try {
+						String dbgmsg = new String(bytes,"ISO-8859-1");
+						
+						
+						Log.e("SERV","SENDING STRING " + dbgmsg + "|size: " + bytes.length);
+						
+						output_writer.write(bytes);
+						//if(dbgmsg.contains("noc")) {
+							//Log.e("SERV","CLOSEING STREAM");
+							//output_writer.flush();
+							//output_writer.flush();
+							//output_writer.close();
+						//}
+
+						output_writer.flush();
+						
+						//Log.e("SERV","calling notify.");
+
+						//output_writer.
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						//e.printStackTrace();
+					//}
+					
+					}
+					break;
+				case MESSAGE_REQUESTBUFFER:
+					//dispatch();
+					try {
+						sendBuffer();
+					} catch (RemoteException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case MESSAGE_SAVEBUFFER:
+					the_buffer = new StringBuffer(msg.getData().getString("BUFFER") + the_buffer);
+					break;
+				case MESSAGE_CHECKIFALIVE:
+					if(the_socket != null) {
+						//the_socket.
+						/*if(!the_socket.isConnected() || the_socket.isClosed()) {
+						//do shutdown, except now, attempt to launch a dialog.
+							Toast.makeText(BaardTERMService.this, "CONNECTION DEAD", 3000);
+						}*/
+					}
+					this.sendEmptyMessageDelayed(BaardTERMService.MESSAGE_CHECKIFALIVE, 3000);
+					break;
+				default:
+					break;	
+				}
+				
+			}
+		};
+		showNotification();
+		//Log.e("SERV","REACHED THE END OF THE STARTUP METHOD");
+		//Looper.loop();
+		myhandler.sendEmptyMessageDelayed(BaardTERMService.MESSAGE_CHECKIFALIVE, 3000);
+		//REGISTER TRIGGER PATTERS.
+		//test_set.add("Your eyes glaze over.");
+		test_set.add("QUEST: You may now quest again.");
+		test_set.add("i love kurt");
+		//test_set.add("TICK");
+		//test_set.add("--> TICK <--");
+	}
+	
+	public void onDestroy() {
+		Log.e("SERV","ON DESTROY CALLED!");
+		doShutdown();
+	}
+	
+	private final IBaardTERMService.Stub mBinder = new IBaardTERMService.Stub() {
+		public void registerCallback(IBaardTERMServiceCallback m) {
+			if(m != null) {
+				if(callbacks.register(m)) {
+					bindCount++;
+					Log.e("SERV","Registering callback, " + bindCount + " now.");
+				}
+			}
+		}
+		
+		public void unregisterCallback(IBaardTERMServiceCallback m)
+		throws RemoteException {
+			if(m != null) {
+				if(callbacks.unregister(m)) {
+					bindCount--;
+					Log.e("SERV","Unregistering callback, " + bindCount + " left.");
+				}
+			}
+	
+		}
+
+		public int getPid() throws RemoteException {
+			return Process.myPid();
+		}
+		
+		
+		public void endXfer() {
+			
+		}
+		
+		public void setNotificationText(CharSequence seq) throws RemoteException {
+			//do some stuff
+			
+		}
+
+		public void initXfer() throws RemoteException {
+			//
+			myhandler.sendEmptyMessage(BaardTERMService.MESSAGE_INIT);
+		}
+
+		public void sendData(byte[] seq) throws RemoteException {
+			//String str = (String)seq;
+			
+			//byte[] bytes = str.getBytes();
+			
+			//TODO: instead of sending the message from the binder object, send myself a message, then dispatch to pump
+			/*Message msg = pump.getHandler().obtainMessage(DataPumper.MESSAGE_SEND);
+			
+			Bundle b = new Bundle();
+			
+			b.putByteArray("THEDATA", seq);
+			
+			msg.setData(b);
+			
+			pump.getHandler().sendMessage(msg);*/
+			
+			//ENTER GIANT SYNCHRONIZATION STEP
+			if(myhandler.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
+			
+				//Log.e("SERV","GOING TO SLEEP");
+			
+				synchronized(sendlock) {
+				
+					while(myhandler.hasMessages(BaardTERMService.MESSAGE_SENDDATA)) {
+						
+						try {
+							sendlock.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				
+				//Log.e("SERV","WAKING BACK UP AFTER NOTIFY CALLED");
+			
+			}
+			
+			Message msg = myhandler.obtainMessage(BaardTERMService.MESSAGE_SENDDATA);
+			
+			Bundle b = new Bundle();
+			
+			b.putByteArray("THEDATA",seq);
+			
+			msg.setData(b);
+			
+			myhandler.sendMessage(msg);
+		
+			
+		}
+		
+		public void setConnectionData(String ihost,int iport) {
+			//host = ihost;
+			//port = iport;
+			Message msg = myhandler.obtainMessage(BaardTERMService.MESSAGE_SETDATA);
+			Bundle b = new Bundle();
+			b.putString("HOST",ihost);
+			b.putInt("PORT",iport);
+			
+			msg.setData(b);
+			
+			myhandler.sendMessage(msg);
+			
+		}
+		
+		public void beginCompression() {
+			myhandler.sendEmptyMessage(BaardTERMService.MESSAGE_STARTCOMPRESS);
+			
+		}
+		
+		public void stopCompression() {
+			
+		}
+
+		public void requestBuffer() throws RemoteException {
+			myhandler.sendEmptyMessage(BaardTERMService.MESSAGE_REQUESTBUFFER);
+			
+		}
+
+		public void saveBuffer(String buffer) throws RemoteException {
+			Message msg = myhandler.obtainMessage(BaardTERMService.MESSAGE_SAVEBUFFER);
+			Bundle b = msg.getData();
+			b.putString("BUFFER",buffer);
+			msg.setData(b);
+			myhandler.sendMessage(msg);
+			
+		}
+
+
+	};
+	
+	Pattern newline = Pattern.compile("\n");
+	
+
+	public Bundle CountNewLine(String ISOLATINstring,int maxlines) {
+		
+		Matcher match = newline.matcher(ISOLATINstring);
+		
+		int prunelocation = 0;
+		int numberfound = 0;
+		while(match.find()) {
+			numberfound++;
+		}
+		
+		if(numberfound > maxlines) {
+			int numtoprune = numberfound - maxlines;
+			match.reset();
+			for(int i = 0;i < numtoprune;i++) {
+				if(match.find()) { //shouldalways be true
+					prunelocation = match.start();
+				}
+			}
+			//by the time we are here, the prunelocation is known
+		}
+		
+		Bundle dat = new Bundle();
+		dat.putInt("TOTAL", numberfound);
+		dat.putInt("PRUNELOC", prunelocation);
+		
+		return dat;
+	}
+	
+	public void sendBuffer() throws RemoteException {
+		final int N = callbacks.beginBroadcast();
+		for(int i = 0;i<N;i++) {
+			//callbacks.getBroadcastItem(i).dataIncoming(data);
+			//callbacks.getBroadcastItem(i).processedDataIncoming(the_buffer);
+			callbacks.getBroadcastItem(i).rawDataIncoming(the_buffer.toString());
+		}
+		
+		callbacks.finishBroadcast();
+		
+		//Log.e("SERV","BUFFERED DATA REQUESTED. Delivered and cleared.");
+		
+		if(the_buffer != null) {
+			the_buffer.setLength(0);
+			//the_buffer.clearSpans();
+		}
+	}
+	
+	Colorizer colorer = new Colorizer();
+	Pattern colordata = Pattern.compile("\\x1B\\x5B(([0-9]{1,2});)?([0-9]{1,2})m");
+	StringBuffer regexp_test = new StringBuffer();
+	Vector<String> test_set = new Vector<String>();
+	
+	public void dispatch(byte[] data) throws RemoteException, UnsupportedEncodingException {
+		
+		String rawData = the_processor.RawProcess(data);
+
+		
+		//Spannable processed = the_processor.DoProcess(data);
+		
+		
+		//String htmlText = colorer.htmlColorize(data);
+		//Log.e("SERV","MADE SOME HTML:"+htmlText);
+		
+		the_buffer.append(rawData);
+		
+		//IDLE:  "Your eyes glaze over."
+		//REQU:  "QUEST: You may now quest again."
+		Matcher stripcolor = colordata.matcher(rawData);
+		regexp_test.append(stripcolor.replaceAll(""));
+		
+		//
+		//test the de-colorized data against registered patterns.
+		Iterator<String> items = test_set.listIterator();
+		while(items.hasNext()) {
+			
+			String test_for = (String)items.next();
+			
+			
+			//Log.e("SERVICE","TESTING FOR:" + test_for + "| agaist:" + regexp_test);
+			//test_for.replaceAll(".", "\\.");
+			//test_for.replaceAll("\"", "\\\""); //yea bitch
+			//test_for.replaceAll("[", substitute)
+			//String regex = "^.*"+Pattern.quote(test_for)+".*$";
+			//String regex = Pattern.quote(test_for);
+			Pattern p = Pattern.compile("^.*"+Pattern.quote(test_for)+".*$",Pattern.DOTALL);
+			
+			//Boolean matched = Pattern.matches(regex, new String(regexp_test.toString().getBytes("ISO-8859-1"),"ISO-8859-1"));
+			//Log.e("SLICK","MATCH: "+regexp_test+"\nAGAINST: " + regex + "\nRETURNED:" + matched);
+			Matcher results = p.matcher(regexp_test);
+			//Matcher results = p.matcher(regexp_test);
+			//results.
+			if(results.matches()) {
+				//NOTIFICATION: 
+				Log.e("SERVICE","SERVICE MATCHED: " + test_for);
+				//build a new notification.
+				Notification note = new Notification(R.drawable.btn_plus,"BaardTERM-Pattern Matched",System.currentTimeMillis());
+				//note.setLatestEventInfo(this, contentTitle, contentText, contentIntent);
+				
+				Context context = getApplicationContext();
+				CharSequence contentTitle = "BaardTERM";
+				//CharSequence contentText = "Hello World!";
+				CharSequence contentText = "Triggerd on: " + test_for;
+				Intent notificationIntent = new Intent(this, com.happygoatstudios.bt.window.BaardTERMWindow.class);
+
+				notificationIntent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+			
+				PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+				
+				note.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+				
+				note.flags = Notification.FLAG_ONLY_ALERT_ONCE | Notification.FLAG_SHOW_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+				note.defaults = Notification.DEFAULT_SOUND;
+				note.ledARGB = 0xFFFF00FF;
+				note.ledOnMS = 300;
+				note.ledOffMS = 300;
+				
+				long[] vp = new long[4];
+				vp[0] = 0;
+				vp[1] = 200;
+				vp[2] = 50;
+				vp[3] = 200;
+				
+				note.vibrate = vp;
+				
+				mNM.notify(trigger_count++,note);
+				
+			}
+		}
+		
+		regexp_test.setLength(0);
+		//the_buffer.append(sstr);
+		/*Bundle datal = CountNewLine(new String(the_buffer.toString().getBytes("ISO-8859-1"),"ISO-8859-1"),100);
+		if(datal.getInt("TOTAL") > 100) {
+			//Log.e("SERV","TIME TO PRUNE ");
+			Object[] unusedspans = the_buffer.getSpans(0, datal.getInt("PRUNELOC"), Object.class);
+			for(int i=0;i<unusedspans.length;i++) {
+				the_buffer.removeSpan(unusedspans[i]);
+			}
+			Object[] array = the_buffer.getSpans(0,the_buffer.length(),Object.class);
+			int size = array.length;
+			//Log.e("SERV","FOUND: " + size + " spans.\n");
+			the_buffer.replace(0, datal.getInt("PRUNELOC"), "");
+			
+		}*/
+		
+		
+		
+		
+		
+		final int N = callbacks.beginBroadcast();
+		int final_count = N;
+		
+
+		
+		for(int i = 0;i<N;i++) {
+			//callbacks.getBroadcastItem(i).dataIncoming(data);
+			//callbacks.getBroadcastItem(i).processedDataIncoming(the_buffer);
+			//callbacks.getBroadcastItem(i).htmlDataIncoming(htmlText);
+			try {
+			callbacks.getBroadcastItem(i).rawDataIncoming(rawData);
+			} catch (RemoteException e) {
+				//just need to catch it, don't need to care, the list maintains itself apparently.
+				final_count = final_count - 1;
+			}
+		}
+		
+		callbacks.finishBroadcast();
+		
+		//if(callbacks.)
+		if(final_count == 0) {
+			//someone is listening so don't save the buffer
+			//Log.e("SERV","No listeners, buffering data.");
+		} else {
+			
+			the_buffer.setLength(0);
+			//the_buffer.clearSpans();
+			//Log.e("SERV","Clearing the buffer because I have " + bindCount + " listeners.");
+		}
+		
+		//callbacks.finishBroadcast();
+		
+	}
+	
+	public void doStartup() throws SocketException {
+		if(host == BAD_HOST || port == BAD_PORT) {
+			return; //dont' start 
+		}
+		
+		InetAddress addr = null;
+		try {
+			//InetAddress[] x = InetAddress.getAllByName(host);
+			addr = InetAddress.getByName(host);
+			//addr = x[0];
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		
+		the_addr = addr;
+		if(the_addr == null) {
+			//Log.e("SERV","NULL ADDRESS LOOKED UP");
+		}
+		
+
+		
+		try {
+			the_socket = new Socket(addr.getHostAddress(),port);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		the_socket.setSendBufferSize(1024);
+		int size = the_socket.getSendBufferSize();
+		boolean shut = the_socket.isOutputShutdown();
+		if(shut) {
+			//Log.e("SERV","SOCKET OUTPUT IS SHUT DOWN");
+		} else {
+			//Log.e("SERV","SOCKET OUTPUT ACTIVE, BUFFER SIZE: " + size);
+		}
+		
+		try {
+			output_writer = new BufferedOutputStream(the_socket.getOutputStream());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//at this point we should have a valid socket
+		//start up the pump
+		//try {
+		//	outputter = new OutputWriter(the_socket.getOutputStream());
+		//} catch (IOException e1) {
+			// TODO Auto-generated catch block
+		//	e1.printStackTrace();
+		//}
+		//outputter.start();
+		try {
+			pump = new DataPumper(the_socket.getInputStream(),null,myhandler);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		pump.start();
+		
+		synchronized(this) {
+			try {
+				this.wait(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //give the pump some time sto start up
+		}
+		
+		pump.getHandler().sendEmptyMessage(DataPumper.MESSAGE_INITXFER);
+		
+		//show notification
+		showNotification();
+		
+		the_processor = new Processor(myhandler,mBinder);
+		the_buffer = new StringBuffer();
+	}
+	
+	private void showNotification() {
+		
+		
+		
+		Notification note = new Notification(R.drawable.btn_plus,"BaardTERM",System.currentTimeMillis());
+		//note.setLatestEventInfo(this, contentTitle, contentText, contentIntent);
+		
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "BaardTERM";
+		//CharSequence contentText = "Hello World!";
+		CharSequence contentText = "Connected: "+ host +":"+ port + ")";
+		Intent notificationIntent = new Intent(this, com.happygoatstudios.bt.window.BaardTERMWindow.class);
+
+		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+		
+		note.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+		
+		note.flags = Notification.FLAG_ONGOING_EVENT;
+		
+		mNM.notify(5545,note);
+		
+	}
+	
+	public void doShutdown() {
+		//pump.stop();
+		if(pump != null) {
+			pump.getHandler().sendEmptyMessage(DataPumper.MESSAGE_END);
+			pump = null;
+		}
+		
+		if(output_writer != null) {
+			try {
+				output_writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+		
+		if(the_socket != null) {
+			try {
+				the_socket.close();
+			} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+			the_socket = null;
+		}
+		//kill the notification.
+		mNM.cancel(5545);
+		
+		
+	}
+	
+	public void doProcess(byte[] data) {
+		//broadcast this data.
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return mBinder;
+	}
+
+}
