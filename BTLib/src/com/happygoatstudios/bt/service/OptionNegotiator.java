1af982b14394dac2a42064ab67f67f3932bd9548
Daniel Block <blockda@gmail.com>
2011-03-30 12:43:32 -0700

    tweak to make discworld work

--- BTLib/src/com/happygoatstudios/bt/service/OptionNegotiator.java
+++ BTLib/src/com/happygoatstudios/bt/service/OptionNegotiator.java
@@ -19,7 +19,7 @@ public class OptionNegotiator {
 	boolean isNAWS=false;
 	//boolean hasNAWSed = false;
 
-	private String[] termtypes = {"BlowTorch","xterm-256color","ansi","UNKNOWN"};
+	private String[] termtypes = {"BlowTorch","ansi","xterm-256color","UNKNOWN"};
 	private int attempt = 0;
 	
 	public OptionNegotiator() {
