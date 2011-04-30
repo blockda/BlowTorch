7590fb4016dc5a93e5faeef1b5f031697bdc0912
Daniel Block <blockda@gmail.com>
2011-04-29 19:01:58 -0700

    More cleanup and default hyperlink color.

--- BTLib/src/com/happygoatstudios/bt/settings/HyperSettings.java
+++ BTLib/src/com/happygoatstudios/bt/settings/HyperSettings.java
@@ -40,7 +40,7 @@ public class HyperSettings {
 			return mode;
 		}
 	}
-	public final static int DEFAULT_HYPERLINK_COLOR = 0xFF8888EE;
+	public final static int DEFAULT_HYPERLINK_COLOR = 0xFF3333AA;
 	
 	private int LineSize = 18;
 	private int LineSpaceExtra = 2;
