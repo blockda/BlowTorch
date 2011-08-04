dad9597f3c8c3eb880d318ae9514ba7e357bcd78
Daniel Block <blockda@gmail.com>
2011-08-04 09:34:02 -0700

    i do believe we have a working lua runtime

---
+++ BTLib/obj/local/armeabi/objs/luajava-1.1/lstate.o.d
@@ -0,0 +1,110 @@
+/cygdrive/e/projects/apps/BlowTorch/BTLib/obj/local/armeabi/objs/luajava-1.1/lstate.o: \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lstate.c \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lua.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/luaconf.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/limits.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/math.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ldebug.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lstate.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lobject.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/llimits.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ltm.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lzio.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lmem.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ldo.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lfunc.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lgc.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/llex.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lstring.h \
+ /cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ltable.h
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lua.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/luaconf.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/math.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/e/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ldebug.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lstate.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lobject.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/llimits.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ltm.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lzio.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lmem.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ldo.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lfunc.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lgc.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/llex.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/lstring.h:
+
+/cygdrive/e/projects/apps/BlowTorch/BTLib/jni/ltable.h:
