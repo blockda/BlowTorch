92b23125e9d3337d77ccdabdc85b37ad6a2dcadc
Daniel Block <blockda@gmail.com>
2011-08-04 09:34:02 -0700

    i do believe we have a working lua runtime

---
+++ BTLib/jni/Android.mk
@@ -0,0 +1,57 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+NDK_APPLICATION_MK=./Application.mk
+LOCAL_MODULE    := luajava-1.1
+LOCAL_SRC_FILES :=  lapi.c \
+	lauxlib.c \
+	lbaselib.c \
+	lcode.c \
+	ldblib.c \
+	ldebug.c \
+	ldo.c \
+	ldump.c \
+	lfunc.c \
+	lgc.c \
+	linit.c \
+	liolib.c \
+	llex.c \
+	lmathlib.c \
+	lmem.c \
+	loadlib.c \
+	lobject.c \
+	lopcodes.c \
+	loslib.c \
+	lparser.c \
+	lstate.c \
+	lstring.c \
+	lstrlib.c \
+	ltable.c \
+	ltablib.c \
+	ltm.c \
+	lundump.c \
+	lvm.c \
+	lzio.c \
+	print.c \
+	luajava.c
+
+				    
+
+
+include $(BUILD_SHARED_LIBRARY)
+
