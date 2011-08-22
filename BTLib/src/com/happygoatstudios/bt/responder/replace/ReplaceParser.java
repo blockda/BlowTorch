9cefc9bbac3eaa9de78e99ae8977065764a10e1f
Laptop <offsetnull@gmail.com>
2011-08-21 23:11:59 -0700

    I do believe we have sequence triggers and replace responders.

---
+++ BTLib/src/com/happygoatstudios/bt/responder/replace/ReplaceParser.java
@@ -0,0 +1,58 @@
+package com.happygoatstudios.bt.responder.replace;
+
+import java.io.IOException;
+
+import org.xml.sax.Attributes;
+import org.xmlpull.v1.XmlSerializer;
+
+import com.happygoatstudios.bt.responder.TriggerResponder;
+import com.happygoatstudios.bt.responder.TriggerResponder.FIRE_WHEN;
+import com.happygoatstudios.bt.service.plugin.settings.BasePluginParser;
+import com.happygoatstudios.bt.service.plugin.settings.PluginSettings;
+import com.happygoatstudios.bt.trigger.TriggerData;
+
+import android.sax.Element;
+import android.sax.TextElementListener;
+
+public final class ReplaceParser {
+	public static void registerListeners(Element root,final PluginSettings settings,final Object obj,final TriggerData current_trigger) {
+		Element r = root.getChild(BasePluginParser.TAG_REPLACERESPONDER);
+		r.setTextElementListener(new TextElementListener() {
+			private ReplaceResponder r = new ReplaceResponder();
+			public void start(Attributes a) {
+				String fireType = a.getValue("",BasePluginParser.ATTR_FIRETYPE);
+				if(fireType == null) fireType = "";
+				//Log.e("PARSER","ACK TAG READ, FIRETYPE IS:" + fireType);
+				if(fireType.equals(TriggerResponder.FIRE_WINDOW_OPEN)) {
+					r.setFireType(TriggerResponder.FIRE_WHEN.WINDOW_OPEN);
+				} else if (fireType.equals(TriggerResponder.FIRE_WINDOW_CLOSED)) {
+					r.setFireType(TriggerResponder.FIRE_WHEN.WINDOW_CLOSED);
+				} else if (fireType.equals(TriggerResponder.FIRE_ALWAYS)) {
+					r.setFireType(TriggerResponder.FIRE_WHEN.WINDOW_BOTH);
+				} else if (fireType.equals(TriggerResponder.FIRE_NEVER)) {
+					r.setFireType(FIRE_WHEN.WINDOW_NEVER);
+				} else {
+					r.setFireType(TriggerResponder.FIRE_WHEN.WINDOW_BOTH);
+				}
+			}
+
+			public void end(String arg0) {
+				//ReplaceResponder r = new ReplaceResponder();
+				r.setWith(arg0);
+				current_trigger.getResponders().add(r.copy());
+			}
+			
+		});
+	}
+
+	public static void saveReplaceResponderToXML(XmlSerializer out,
+			ReplaceResponder r) throws IllegalArgumentException, IllegalStateException, IOException {
+		out.startTag("", BasePluginParser.TAG_REPLACERESPONDER);
+		out.attribute("", BasePluginParser.ATTR_FIRETYPE, r.getFireType().getString());
+		
+		out.text(r.getWith());
+		
+		out.endTag("", BasePluginParser.TAG_REPLACERESPONDER);
+		
+	}
+}
--- BTLib/src/com/happygoatstudios/bt/trigger/TriggerParser.java
+++ BTLib/src/com/happygoatstudios/bt/trigger/TriggerParser.java
@@ -9,6 +9,7 @@ import org.xmlpull.v1.XmlSerializer;
 import com.happygoatstudios.bt.responder.TriggerResponder;
 import com.happygoatstudios.bt.responder.ack.AckResponderParser;
 import com.happygoatstudios.bt.responder.notification.NotificationResponderParser;
+import com.happygoatstudios.bt.responder.replace.ReplaceParser;
 import com.happygoatstudios.bt.responder.script.ScriptResponderParser;
 import com.happygoatstudios.bt.responder.toast.ToastResponderParser;
 import com.happygoatstudios.bt.service.plugin.settings.BasePluginParser;
@@ -32,7 +33,9 @@ public final class TriggerParser {
 				current_trigger.setFireOnce(a.getValue("",BasePluginParser.ATTR_TRIGGERONCE).equals("true") ? true : false);
 				current_trigger.setHidden( (a.getValue("",BasePluginParser.ATTR_TRIGGERHIDDEN) == null) ? false : (a.getValue("",BasePluginParser.ATTR_TRIGGERHIDDEN)).equals("true") ? true : false);
 				current_trigger.setEnabled( (a.getValue("",BasePluginParser.ATTR_TRIGGERENEABLED) == null) ? true : (a.getValue("",BasePluginParser.ATTR_TRIGGERENEABLED)).equals("true") ? true : false);
-				
+				current_trigger.setSequence( (a.getValue("",BasePluginParser.ATTR_SEQUENCE) == null) ? 10 : Integer.parseInt(a.getValue("",BasePluginParser.ATTR_SEQUENCE)));
+				current_trigger.setGroup( (a.getValue("",BasePluginParser.ATTR_GROUP) == null) ? "" : a.getValue("",BasePluginParser.ATTR_GROUP));
+				current_trigger.setKeepEvaluating((a.getValue("",BasePluginParser.ATTR_KEEPEVALUATING) == null) ? true : ("true".equals(a.getValue("",BasePluginParser.ATTR_KEEPEVALUATING))) ? true : false);
 				current_trigger.setResponders(new ArrayList<TriggerResponder>());
 				
 			}
@@ -51,6 +54,7 @@ public final class TriggerParser {
 		ToastResponderParser.registerListeners(trigger, settings, obj, current_trigger, current_timer);
 		NotificationResponderParser.registerListeners(trigger, settings, obj, current_trigger, current_timer);
 		ScriptResponderParser.registerListeners(trigger, settings, obj, current_trigger, current_timer);
+		ReplaceParser.registerListeners(trigger, settings, new TriggerData(), current_trigger);
 	}
 	
 	public static void saveTriggerToXML(XmlSerializer out,TriggerData trigger) throws IllegalArgumentException, IllegalStateException, IOException {
@@ -62,6 +66,9 @@ public final class TriggerParser {
 			out.attribute("", BasePluginParser.ATTR_TRIGGERONCE, trigger.isFireOnce() ? "true" : "false");
 			if(trigger.isHidden())  out.attribute("", BasePluginParser.ATTR_TRIGGERHIDDEN, "true");
 			out.attribute("", BasePluginParser.ATTR_TRIGGERENEABLED, trigger.isEnabled() ? "true" : "false");
+			out.attribute("", BasePluginParser.ATTR_SEQUENCE, Integer.toString(trigger.getSequence()));
+			if(!trigger.getGroup().equals(TriggerData.DEFAULT_GROUP)) out.attribute("", BasePluginParser.ATTR_GROUP, trigger.getGroup());
+			out.attribute("", BasePluginParser.ATTR_KEEPEVALUATING, trigger.isKeepEvaluating() ? "true" : "false");
 			
 			for(TriggerResponder r : trigger.getResponders()){
 				r.saveResponderToXML(out);
