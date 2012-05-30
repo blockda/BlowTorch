6f03cfc389b8301999809d6625f2fde76dce32b0
Daniel Block <blockda@gmail.com>
2012-05-29 21:25:13 -0700

    O3 changed, attempting to add the arm-v7a libs

--- BTLib/jni/Application.mk
+++ BTLib/jni/Application.mk
@@ -1,3 +1,3 @@
-APP_ABI := armeabi 
-APP_CFLAGS := -O2 -fno-auto-inc-dec
+APP_ABI := armeabi armeabi-v7a 
+APP_CFLAGS := -O3 -fno-auto-inc-dec
 APP_MODULES := lsqlite3 sqlite3 lua bit marshal luabins
