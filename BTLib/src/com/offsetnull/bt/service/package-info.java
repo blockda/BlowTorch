30fd6b03a2b50b41cd969b8072e333e920ec8ab5
Daniel Block <blockda@gmail.com>
2013-04-02 21:52:09 -0700

    Fixing up the checkstyle config

---
+++ BTLib/src/com/offsetnull/bt/service/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) Dan Block 2013
+ */
+/**
+ * Core classes to provide background connections
+ * and telnet communications.
+ *
+ * @since always
+ * @see the-bigger-picture
+ */
+package com.offsetnull.bt.service;
