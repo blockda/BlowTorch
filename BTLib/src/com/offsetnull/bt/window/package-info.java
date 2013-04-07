79db342a943c99f322ae0a13835df69f4ae4c6ab
Dan Block (macbook) <blockda@gmail.com>
2013-04-07 11:38:31 -0700

    Cleanup of the window package has begun. Going to test some things first before totally axing the increased thread priority from the code.

---
+++ BTLib/src/com/offsetnull/bt/window/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) Dan Block 2013
+ */
+/**
+ * Core classes that provide the foreground UI that can be interacted with.
+ * Mainly windowing, miniwindowing and ansi drawing.
+ * Editors for data such as Aliases or Triggers can be found in the corresponding package.
+ *
+ * @since always
+ * @see the-bigger-picture
+ */
+package com.offsetnull.bt.window;
