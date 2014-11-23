db0d4598b117fc717e0c880ea143b36fa17c32ec
Dan Block (macbook) <blockda@gmail.com>
2014-11-23 11:47:51 -0800

    NDK Build system is almost complete.

--- BTLib/jni/lmarshal/Android.mk
+++ BTLib/jni/lmarshal/Android.mk
@@ -4,9 +4,9 @@ ifeq ($(TARGET_ARCH_ABI),armeabi)
 include $(CLEAR_VARS)
 LOCAL_MODULE := marshal
 LOCAL_MODULE_FILENAME := marshal
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../luajava
 LOCAL_SRC_FILES := ./lmarshal.c
-LOCAL_SHARED_LIBRARIES := liblua
+LOCAL_SHARED_LIBRARIES := lua
 LOCAL_CFLAGS := -O3 -fpic -std=c99 -shared
 include $(BUILD_SHARED_LIBRARY)
 
@@ -16,9 +16,9 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 include $(CLEAR_VARS)
 LOCAL_MODULE := marshal
 LOCAL_MODULE_FILENAME := marshal
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../luajava
 LOCAL_SRC_FILES := ./lmarshal.c
-LOCAL_SHARED_LIBRARIES := liblua
+LOCAL_SHARED_LIBRARIES := lua
 LOCAL_CFLAGS := -O3 -fpic -std=c99 -shared
 include $(BUILD_SHARED_LIBRARY)
 endif
--- BTLib/jni/lsqlite3/Android.mk
+++ BTLib/jni/lsqlite3/Android.mk
@@ -4,9 +4,9 @@ ifeq ($(TARGET_ARCH_ABI),armeabi)
 include $(CLEAR_VARS)
 LOCAL_MODULE := lsqlite3
 LOCAL_MODULE_FILENAME := lsqlite3
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3 $(LOCAL_PATH)/../lua
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3 $(LOCAL_PATH)/../luajava
 LOCAL_SRC_FILES := ./lsqlite3.c
-LOCAL_SHARED_LIBRARIES := sqlite3 liblua
+LOCAL_SHARED_LIBRARIES := sqlite3 lua 
 LOCAL_CFLAGS := -O3 -fpic -std=c99 -shared
 include $(BUILD_SHARED_LIBRARY)
 
@@ -16,9 +16,9 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 include $(CLEAR_VARS)
 LOCAL_MODULE := lsqlite3
 LOCAL_MODULE_FILENAME := lsqlite3
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3 $(LOCAL_PATH)/../lua
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3 $(LOCAL_PATH)/../luajava
 LOCAL_SRC_FILES := ./lsqlite3.c
-LOCAL_SHARED_LIBRARIES := sqlite3 liblua
+LOCAL_SHARED_LIBRARIES := sqlite3 lua
 LOCAL_CFLAGS := -O3 -fpic -std=c99 -shared
 include $(BUILD_SHARED_LIBRARY)
 endif
