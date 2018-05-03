5f24face29c83779e2c050eff003b2135a1d491b
Dan Block (macbook) <blockda@gmail.com>
2018-05-03 10:54:02 -0700

    small cleanup effort

--- BT_Free/makeshift.sh
+++
@@ -1,8 +0,0 @@
-ant clean
-python update_manifest.py
-cp -rv ../../btscripts/button_ex/src/ ./assets/share/lua/5.1/
-rm -rf ./assets/share/lua/5.1/button_window_ex.xml 
-python integrate_button_ex.py 
-ant release
-adb install -r bin/BlowTorch-release.apk
-adb shell am start -n com.happygoatstudios.bt/com.offsetnull.btfree.FreeLauncher 
