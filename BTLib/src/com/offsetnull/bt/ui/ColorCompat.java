616ef2b1f3dc7d5582b0a57a5e526a2eb53506d7
Dan Block (macbook) <blockda@gmail.com>
2018-05-04 18:01:37 -0700

    Fixing problems with the button window and oreo+ or whatever.

---
+++ BTLib/src/com/offsetnull/bt/ui/ColorCompat.java
@@ -0,0 +1,9 @@
+package com.offsetnull.bt.ui;
+
+public class ColorCompat {
+
+    public static int argb(int a, int r, int g, int b) {
+        return android.graphics.Color.argb(a,r,g,b);
+    }
+
+}
